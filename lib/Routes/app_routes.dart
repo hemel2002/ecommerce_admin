4f507b290c4a8ae3457d6e34f605f500812091e8
Shahriar Kabir Hemal <shahriar12688@gmail.com>
2025-07-13 02:05:45 +0600

    fix error

---
+++ lib/Routes/app_routes.dart
@@ -0,0 +1,16 @@
+import 'package:ecommerce_admin_panel/Routes/routes.dart';
+import 'package:ecommerce_admin_panel/features/authentication/screens/Reset_password/Responsive_screens/reset_password.dart';
+import 'package:ecommerce_admin_panel/features/authentication/screens/Forget_password/Responsive_screens/forget_password.dart';
+import 'package:ecommerce_admin_panel/features/authentication/screens/login/login.dart';
+import 'package:get/get.dart';
+
+class TApproute {
+  static final List<GetPage> pages = [
+    // Add your app routes here
+    // Example:
+    // GetPage(name: TRoutes.home, page: () => HomeScreen()),
+    GetPage(name: TRoutes.login, page: () => AdminLoginScreen()),
+    GetPage(name: TRoutes.forgotPassword, page: () => ForgetScreen()),
+    GetPage(name: TRoutes.resetPassword, page: () => ResetScreen()),
+  ];
+}
